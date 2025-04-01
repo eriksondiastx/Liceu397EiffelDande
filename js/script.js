caf5dd3a20f59699cd8c7ea30c70f5ba681af77a
eriksondiastx <eriksondiastx@outlook.com>
2025-04-01 22:45:49 +0100

    Melhorias-Pag-Galeria de vídeos

--- js/script.js
+++ js/script.js
@@ -20,3 +20,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+/*GALERIA DE VÍDEOS*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const videos = document.querySelectorAll(".lightbox-trigger");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxVideo = document.getElementById("lightbox-video");
+  const closeLightbox = document.querySelector(".close-lightbox");
+
+  videos.forEach((video) => {
+    video.addEventListener("click", function () {
+      lightbox.style.display = "flex";
+      lightboxVideo.src = this.querySelector("source").src;
+    });
+  });
+
+  closeLightbox.addEventListener("click", function () {
+    lightbox.style.display = "none";
+    lightboxVideo.pause();
+  });
+
+  lightbox.addEventListener("click", function (e) {
+    if (e.target === lightbox) {
+      lightbox.style.display = "none";
+      lightboxVideo.pause();
+    }
+  });
+});
