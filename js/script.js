f7a1c2d2ce45188743dcb308e2163a70b16ccd5a
eriksondiastx <eriksondiastx@outlook.com>
2025-04-02 22:58:19 +0100

    Melhorias-Pag-Noticias_Concurso_Kandengue-Abrir imagem em tela cheia,Efeito zoom ao passar o mouse sobre a imagem, botão para fechar imagem em tela cheia

--- js/script.js
+++ js/script.js
@@ -48,3 +48,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Seleciona os elementos
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeLightbox = document.querySelector(".close-lightbox");
+const images = document.querySelectorAll(".lightbox-trigger");
+
+// Abre o Lightbox ao clicar na imagem
+images.forEach((img) => {
+  img.addEventListener("click", function () {
+    lightbox.style.display = "flex";
+    lightboxImg.src = this.src;
+  });
+});
+
+// Fecha o Lightbox ao clicar no botão de fechar
+closeLightbox.addEventListener("click", function () {
+  lightbox.style.display = "none";
+});
+
+// Fecha o Lightbox ao clicar fora da imagem
+lightbox.addEventListener("click", function (e) {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
